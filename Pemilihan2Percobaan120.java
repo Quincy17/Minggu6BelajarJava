a00a76456548a7ffa0cd0d970a65a49d8897afb8
FarrelCaesarian <farrelcaesarian15@gmail.com>
2023-10-02 13:38:47 +0700

    update pemilihan 2 part 2

--- Pemilihan2Percobaan120.java
+++ Pemilihan2Percobaan120.java
@@ -6,8 +6,10 @@ public class Pemilihan2Percobaan120{
         System.out.print("Masukkan Tahun : ");
         int tahun = input20.nextInt();
         if((tahun%4)==0){
-            if((tahun%100)==0 && (tahun%400)==0){
-                System.out.println("Tahun Kabisat");
+            if((tahun%100)==0){
+                if((tahun%400==0)){
+                    System.out.println("Tahun Kabisat");
+                }
             }
             else if((tahun%100)!=0){
                 System.out.println("Tahun Kabisat");
